ae4e30830d084a9e5efafc988109e74b241ca790
LoTwT <709937065@qq.com>
2021-12-04 14:56:57 +0800

    feat(Content): show codes

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,11 +1,22 @@
-import { Store } from "@/types/store"
+import { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
+
+import "./index.less"
+import { Store } from "@/types/store"
+import { invokePre } from "@/libs/channel"
 
 export default () => {
   const dispatch = useDispatch()
   const currTab = useSelector((state: Store) => state.currTab)
+  const [codes, setCodes] = useState("")
+
+  useEffect(() => {
+    invokePre<string>("readFile", currTab).then(setCodes)
+  }, [currTab])
 
   return currTab ? (
-    <div className="content">{currTab}</div>
+    <div className="content">
+      <textarea value={codes}></textarea>
+    </div>
   ) : null
 }
