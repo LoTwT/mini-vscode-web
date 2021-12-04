7ad6015b2cef1871c27f6c708969119ff1a35c9c
LoTwT <709937065@qq.com>
2021-12-04 10:41:36 +0800

    feat(Tabs): tab doClose

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -11,6 +11,25 @@ export default () => {
   const currTab = useSelector((state: Store) => state.currTab)
 
   const onChangeTab = (newTab: string) => dispatch({ type: ACTION_MAP.SET_CURR_TAB, value: newTab })
+  const doClose = (
+    ev: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+    tab: string
+  ) => {
+    // 阻止冒泡
+    ev.stopPropagation()
+
+    const index = openTabs.indexOf(tab)
+    if (index === -1) return
+
+    // 移除 openTabs 中要关闭的 tab
+    const newTabs = [...openTabs]
+    newTabs.splice(index, 1)
+    dispatch({ type: ACTION_MAP.SET_OPEN_TABS, value: newTabs })
+
+    // 如果要关闭的 tab 是 currTab，重新设置 currTab
+    // todo 重设 currTab 规则
+    currTab === tab && dispatch({ type: ACTION_MAP.SET_CURR_TAB, value: newTabs[newTabs.length - 1] })
+  }
 
   return openTabs.length !== 0 ? (
     <div className="tabs">
@@ -20,7 +39,8 @@ export default () => {
           key={tab}
           onClick={() => onChangeTab(tab)}
         >
-          {getFileName(tab)}
+          <span className="label">{getFileName(tab)}</span>
+          <span className="close-btn" onClick={(ev) => doClose(ev, tab)}>x</span>
         </div>
       ))}
     </div>
